2b46eb6973eba0c332cc03d31382b64010778b64
Tim EKPB <timkpb2023@lampungprov.go.id>
2025-03-21 20:29:56 +0700

    fix useSearchParam

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
 import { Toaster } from 'sonner'
+import { Suspense } from 'react'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -29,7 +30,9 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <Suspense fallback={<div>Loading search...</div>}>
+            {children}
+          </Suspense>
         </ThemeProvider>
         <Toaster />
       </body>
